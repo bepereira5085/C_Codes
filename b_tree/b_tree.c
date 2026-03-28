05a4e21585443f973e52248253500c9a28841511
Bernardo <bernardopcandido@gmail.com>
2026-03-28 16:02:36 -0300

    feat: final implementation for even and odd degree

--- b_tree/b_tree.c
+++ b_tree/b_tree.c
@@ -3,7 +3,8 @@
 #include "b_tree.h"
 
 #define MAX_KEYS (DEGREE-1)
-#define MIN_KEYS ((DEGREE/2)-1)
+#define MIN_DEGREE ((DEGREE + 1) / 2)
+#define MIN_KEYS (MIN_DEGREE - 1)
 
 Node *create_node(int is_leaf) {
     Node *p = (Node *)calloc(1, sizeof(Node));
@@ -21,30 +22,36 @@ Node *create_node(int is_leaf) {
 }
 
 Node *split_node(Node *parent, Node *left_node, int left_node_index) {
-    Node *right_node = (Node *)calloc(1, sizeof(Node));
-    right_node->keys_amount = MIN_KEYS;
-    right_node->is_leaf = left_node->is_leaf;
+    int mid = MAX_KEYS / 2;
 
-    for (int i = 0; i < MIN_KEYS; i++) {
-        right_node->keys[i] = left_node->keys[MIN_KEYS + 1 + i];
+    Node *right_node = create_node(left_node->is_leaf);
+
+    right_node->keys_amount = MAX_KEYS - mid - 1;
+
+    for (int i = 0; i < right_node->keys_amount; i++) {
+        right_node->keys[i] = left_node->keys[mid + 1 + i];
     }
 
     if (!left_node->is_leaf) {
-        for (int i = 0; i < MIN_KEYS + 1; i++) {
-            right_node->children[i] = left_node->children[MIN_KEYS + 1 + i];
+        for (int i = 0; i <= right_node->keys_amount; i++) {
+            right_node->children[i] = left_node->children[mid + 1 + i];
+            left_node->children[mid + 1 + i] = NULL;
         }
     }
 
-    left_node->keys_amount = MIN_KEYS;
-    for (int i = parent->keys_amount; i >= left_node_index + 1; i--) {
-        parent->children[i+1] = parent->children[i];
+    left_node->keys_amount = mid;
+
+    for (int i = parent->keys_amount; i > left_node_index; i--) {
+        parent->children[i + 1] = parent->children[i];
     }
+
     parent->children[left_node_index + 1] = right_node;
 
     for (int i = parent->keys_amount - 1; i >= left_node_index; i--) {
-        parent->keys[i+1] = parent->keys[i];
+        parent->keys[i + 1] = parent->keys[i];
     }
-    parent->keys[left_node_index] = left_node->keys[MIN_KEYS];
+
+    parent->keys[left_node_index] = left_node->keys[mid];
     parent->keys_amount++;
 
     return right_node;
@@ -64,7 +71,7 @@ void insert_on_node(Node *node, int val) {
             i--;
         } i++;
 
-        if (node->children[i]->keys_amount == MAX_KEYS) {
+        if (node->children[i] != NULL && node->children[i]->keys_amount == MAX_KEYS) {
             split_node(node, node->children[i], i);
             if (node->keys[i] < val) {
                 i++;
--- b_tree/main.c
+++ b_tree/main.c
@@ -3,13 +3,10 @@
 #include <stdlib.h>
 #include "b_tree.h"
 
-#define ARRAY_SIZE 4
-
 int main() {
     Node *root = NULL;
 
     int i = 0;
-    // int numbers[ARRAY_SIZE];
 
     root = insert_val(root, 10);
     print_tree(root);
@@ -42,60 +39,7 @@ int main() {
     print_tree(root);
     printf("\n");
 
-    // uncomment this line if yoy want to change the tree numbers randomly
-    // srand(time(NULL));
-
-    // while (i < ARRAY_SIZE) {
-    // numbers[i] = rand() % 1000;
-    //     i++;
-    // }
-
-    // for (int i = 0; i < ARRAY_SIZE; i++) {
-    //     root = insert_val(root, numbers[i]);
-    // }
-
     print_tree(root);
-
-    // int search_number = 827;
-    // Node *found_node = search_val(root, search_number);
-
-    // if (found_node == NULL) {
-    //     printf("\no valor nao esta na arvore\n");
-    // } else {
-    //     printf("--- No ---\n");
-    //     printf("folha? %s\n", found_node->is_leaf ? "sim" : "nao");
-    //     printf("valores quantidade: %d\n", found_node->keys_amount);
-
-    //     printf("chaves: [");
-    //     for (int i = 0; i < found_node->keys_amount; i++) {
-    //         printf("%d", found_node->keys[i]);
-    //         if (i < found_node->keys_amount - 1) {
-    //             printf(", ");
-    //         }
-    //     }
-    //     printf("]\n");
-
-    //     printf("ponteiros: [");
-    //     int last_index = 0;
-    //     for (int i = 0; i < found_node->keys_amount; i++) {
-    //         printf("%p", (void *)found_node->children[i]);
-    //         if (i <= found_node->keys_amount - 1) {
-    //             printf(", ");
-    //         }
-    //         last_index++;
-    //     }
-    //     printf("%p]\n\n", (void *)found_node->children[last_index]);
-
-    //     printf("infos valor encontrado:\n");
-    //     int val_index;
-    //     for(int i = 0; i < found_node->keys_amount; i++) {
-    //         if (found_node->keys[i] == search_number) {
-    //             val_index = i;
-    //             break;
-    //         }
-    //     }
-    //     printf("%p <-- %d --> %p", (void *)found_node->children[val_index], search_number, (void *)found_node->children[val_index+1]);
-    // }
 
     dealloc_tree(root);
 
